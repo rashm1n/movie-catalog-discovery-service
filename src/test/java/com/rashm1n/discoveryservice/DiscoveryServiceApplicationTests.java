4f0a6179e9540badb5a7ca4fb6d8254f7056584b
rashm1n <rashmin.ravindu@gmail.com>
2022-02-17 02:06:57 +0530

    Initial commit

---
+++ src/test/java/com/rashm1n/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rashm1n.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class DiscoveryServiceApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
